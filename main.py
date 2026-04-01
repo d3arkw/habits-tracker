240f1b93e4a1e811b4e237514840c93488981c9b
d3arkw <d3arkw@icloud.com>
2026-04-01 23:39:34 +0300

    add multi-language and fix code

--- main.py
+++ main.py
@@ -2,19 +2,73 @@ import json
 import os
 from datetime import datetime
 
-data = {"habits": []}
-index = 0
-
 
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
 
-def chek_len():
+data = {"habits": []}
+sel_language = "en"
+
+
+language = {
+    "ru": {
+        "prin": "Добро пожаловать в трекер привычек!",
+        "input_int": "Введите число!",
+        "clear": "Список очищен",
+        "cleaned": "Список и так пуст!",
+        "done": "Уже выполнено!",
+        "add_habit": "Введите название привычки:",
+        "input_num": "Введите номер: ",
+        "inp_correct": "Введите верное число!",
+        "streak": "Серия:",
+        "list_clean": "Список пуст",
+        "done_yet": "Уже выполнялось сегодня!",
+        "inp_num_habbit": "Введите номер выполненной привычки:",
+        "menu": "\nМеню:\n1.Добавить привычку\n2.Удалить привычку\n3.Список привычек\n4.Отметить выполнение\n5.Очистить все привычки\n6.Выход",
+        "sel_menu": "Выберите пункт меню:",
+        "bye": "До свидания!",
+    },
+    "en": {
+        "prin": "Welcome to the habit tracker!",
+        "input_int": "Enter a number!",
+        "clear": "List cleared",
+        "cleaned": "The list is already empty!",
+        "done": "Already done!",
+        "add_habit": "Enter the habit name:",
+        "input_num": "Enter the number: ",
+        "inp_correct": "Enter a valid number!",
+        "streak": "Streak:",
+        "list_clean": "List is empty",
+        "done_yet": "Already completed today!",
+        "inp_num_habbit": "Enter the number of the completed habit:",
+        "menu": "\nMenu:\n1. Add habit\n2. Delete habit\n3. Habit list\n4. Mark as completed\n5. Clear all habits\n6. Exit",
+        "sel_menu": "Select a menu item:",
+        "bye": "Goodbye!",
+    },
+}
+
+
+def translate(key):
+    return language[sel_language][key]
+
+
+def sel_lang(sel_language):
+    print("Select language(Выберите язык):\n1:Russian(Русский)\n2:English(Английский)")
+    d = int(input(translate("input_num")))
+    sel_language = "ru" if d == 1 else "en" if d == 2 else "en"
+    clear()
+    return sel_language
+
+
+sel_language = sel_lang(sel_language)
+
+
+def has_habits(data):
     return len(data["habits"]) > 0
 
 
-def chek_ent(index):
+def is_valid(data, index):
     return 0 <= index < len(data["habits"])
 
 
@@ -24,7 +78,7 @@ def input_tr(a):
             b = int(input(a))
             return b
         except ValueError:
-            print("Введите Число!")
+            print(translate("input_int"))
 
 
 def dump(data, filename="myp"):
@@ -45,20 +99,20 @@ def load(filename="myp"):
 
 def clear_json(data, filename="myp"):
     clear()
-    if chek_len():
+    if has_habits(data):
         with open(filename, "w", encoding="utf-8") as f:
             data = {"habits": []}
             dump(data)
-        print("Список очищен")
+        print(translate("clear"))
     else:
-        print("Список и так пуст!")
+        print(translate("cleaned"))
     return data
 
 
-def get_last(data, index):
+def mark_done(data, index):
     now = datetime.now().strftime("%Y.%m.%d")
     if data["habits"][index]["last_done"] == now:
-        print("Уже выполнено!!")
+        print(translate("done"))
     else:
         update_streak(data, index)
         data["habits"][index]["last_done"] = now
@@ -66,36 +120,36 @@ def get_last(data, index):
 
 def add_habit(data):
     clear()
-    name = input("Введите название привычки:")
+    name = input(translate("add_habit"))
     data["habits"].append({"name": name, "streak": 0, "last_done": None})
     dump(data)
     return data
 
 
 def del_habit(data):
-    if chek_len():
+    if has_habits(data):
         try:
-            index = int(input("Введите номер: ")) - 1
-            if chek_ent(index):
+            index = int(input(translate("input_num"))) - 1
+            if is_valid(data, index):
                 data["habits"].pop(index)
                 return data
             else:
-                print("Введите верное число!")
+                print(translate("inp_correct"))
         except (ValueError, TypeError):
-            print("Введите число!")
+            print(translate("input_int"))
     return data
 
 
 def lst_habits(data):
     clear()
     now = datetime.now().strftime("%Y.%m.%d")
-    if chek_len():
+    if has_habits(data):
         for i, d in enumerate(data["habits"], start=1):
             print(
-                f"{i}. {d['name']} | Серия: {d['streak']} | {'Выполнено' if d['last_done'] == now else 'Не выполнено'}"
+                f"{i}. {d['name']} | {translate("streak")} {d['streak']} | {'✅' if d['last_done'] == now else '❌'}"
             )
     else:
-        print("Список пуст")
+        print(translate("list_clean"))
     return data
 
 
@@ -111,36 +165,35 @@ def update_streak(data, index):
     elif diff.days > 1:
         data["habits"][index]["streak"] = 1
     elif diff.days == 0:
-        print("Уже выполнялось сегодня!")
+        print(translate("done_yet"))
     return data
 
 
 def get_done(data):
+    clear()
     lst_habits(data)
-    if chek_len():
+    if has_habits(data):
         try:
-            index = int(input("Введите номер выполненной привычки:")) - 1
-            if chek_ent(index):
-                get_last(data, index)
+            index = int(input(translate("inp_num_habbit"))) - 1
+            if is_valid(data, index):
+                mark_done(data, index)
             else:
-                print("Введите верное число!")
+                print(translate("inp_correct"))
         except (ValueError, TypeError):
-            print("Введите число!")
+            print(translate("input_int"))
         dump(data)
     return data
 
 
 def menu():
-    print(
-        "\nМеню:\n1.Добавить привычку\n2.Удалить привычку\n3.Список привычек\n4.Отметить выполнение\n5.Очистить все привычки\n6.Выход"
-    )
+    print(translate("menu"))
 
 
-print("Добро пожаловать в трекер привычек!")
+print(translate("prin"))
 data = load()
 while True:
     menu()
-    sel = input_tr("Выберите пункт меню:")
+    sel = input_tr(translate("sel_menu"))
     if sel == 1:
         data = add_habit(data)
     elif sel == 2:
@@ -153,5 +206,6 @@ while True:
     elif sel == 5:
         data = clear_json(data)
     elif sel == 6:
-        print("До свидания!")
+        clear()
+        print(translate("bye"))
         break
